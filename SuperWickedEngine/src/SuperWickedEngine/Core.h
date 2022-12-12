048490745213a6dd57bb5902f3307873dfb7ffee
shimmieblum <shimmieblum@gmail.com>
2022-12-12 23:08:03 +0000

    created initial entry point

---
+++ SuperWickedEngine/src/SuperWickedEngine/Core.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#ifdef SWE_PLATFORM_WINDOWS
+    #ifdef SWE_BUILD_DLL
+        #define SWE_API __declspec(dllexport)
+    #else
+        #define SWE_API __declspec(dllimport)
+    #endif
+#else
+    #error SuperWickedEngine only works on windows
+#endif
+
+
+class Core
+{
+public:
+    
+};

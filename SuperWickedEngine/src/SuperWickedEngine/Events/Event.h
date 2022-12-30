e6e017fb8f2344cad8f7ff3de71ee5df12f211cd
shimmieblum <shimmieblum@gmail.com>
2022-12-30 13:51:27 +0000

    added events for windows abstraction events class

---
+++ SuperWickedEngine/src/SuperWickedEngine/Events/Event.h
@@ -0,0 +1,82 @@
+﻿#pragma once
+#include "../Core.h"
+#include <string>
+#include <functional>
+
+namespace SuperWickedEngine
+{
+    // currently using blocking events rather than buffered events
+    // this is easier to implement but may be improved in the future
+    enum class EventType
+    {
+        None = 0,
+        WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+        MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled,
+        AppTick, AppUpdate, AppRender,
+        KeyPressed, KeyReleased
+    };
+
+    enum EventCategory
+    {
+        None = 0,
+        EventCategoryApplication = BIT(0),
+        EventCategoryInput = BIT(1),
+        EventCategoryKeyboard = BIT(2),
+        EventCategoryMouse = BIT(3),
+        EventCategoryMouseButton = BIT(4)
+    };
+
+#define EVENT_CLASS_TYPE(type)  static EventType GetStaticType() { return EventType::##type; }\
+                                virtual EventType GetEventType() const override { return GetStaticType(); }\
+                                virtual const char* GetName() const override { return #type; }
+
+#define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override { return category; }
+    
+    class SWE_API Event
+    {
+        friend class EventDispatcher;
+    public:
+        [[nodiscard]]
+        virtual EventType GetEventType() const = 0;
+        [[nodiscard]]
+        virtual const char* GetName() const = 0;
+        [[nodiscard]]
+        virtual int GetCategoryFlags() const = 0;
+        [[nodiscard]]
+        virtual std::string ToString() const { return GetName(); }
+
+        bool IsInCategory(EventCategory category) const
+        {
+            return  GetCategoryFlags() & category;
+        }
+    protected:
+        bool m_Handled = false;
+    };
+
+    class EventDispatcher
+    {
+        template<typename T>
+        using EventFn = std::function<bool(T&)>;
+    public:
+        explicit EventDispatcher(Event& event) : m_Event(event) { }
+        
+        template<typename T>
+        bool Dispatch(EventFn<T> func)
+        {
+            if(m_Event.GetEventType() == T::GetStaticType())
+            {
+                m_Event.m_Handled = func(*static_cast<T*>(&m_Event));
+                return true;
+            }
+            return false;
+        }
+
+    private:
+        Event& m_Event;
+    };
+
+    inline std::ostream& operator<<(std::ostream& os, const Event& e)
+    {
+        return os << e.ToString();
+    }
+}
---
+++ SuperWickedEngine/src/SuperWickedEngine/Events/MouseEvent.h
@@ -0,0 +1,98 @@
+﻿#pragma once
+#include <string>
+#include <functional>
+#include <sstream>
+
+#include "Event.h"
+
+namespace SuperWickedEngine
+{
+    class SWE_API MouseEvent : public Event
+    {
+    public:
+        EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
+    protected:
+        MouseEvent() = default;
+    };
+    
+    class SWE_API MouseMovedEvent : public MouseEvent
+    {
+    public:
+        MouseMovedEvent(float x, float y) : m_MouseX(x), m_MouseY(y) {}
+
+        float GetMouseX() const {return  m_MouseX; }
+        float GetMouseY() const { return m_MouseY; }
+
+        std::string ToString() const override
+        {
+            std::stringstream ss;
+            ss << "MouseMovedEvent: " << m_MouseX << ", " << m_MouseY;
+            return ss.str();
+        }
+        EVENT_CLASS_TYPE(MouseMoved)
+    private:
+        float m_MouseX, m_MouseY;
+    };
+
+    class SWE_API MouseScrolledEvent : public MouseEvent
+    {
+    public:
+        MouseScrolledEvent(float xOffset, float yOffset) : m_XOffset(xOffset), m_YOffset(yOffset) {}
+
+        float GetXOffset() const { return m_XOffset; }
+        float GetYOffset() const { return m_YOffset; }
+
+        EVENT_CLASS_TYPE(MouseScrolled)
+    private:
+        float m_XOffset, m_YOffset;
+    };
+
+    class SWE_API MouseButtonEvent : public MouseEvent
+    {
+    public:
+
+        std::string ToString() const override
+        {
+            std::stringstream ss;
+            ss << "MouseButtonEvent button: " << m_Button;
+            return ss.str();
+        }
+
+        int GetButton() const { return  m_Button; }
+
+    protected:
+        MouseButtonEvent(int button): m_Button(button) {}
+        
+        int m_Button;
+    };
+
+    class SWE_API MouseButtonPressedEvent : public MouseButtonEvent
+    {
+    public:
+        MouseButtonPressedEvent(int button) : MouseButtonEvent(button) {}
+
+        std::string ToString() const override
+        {
+            std::stringstream ss;
+            ss << "MouseButtonPressedEvent: " << m_Button;
+            return ss.str();
+        }
+
+        EVENT_CLASS_TYPE(MouseButtonPressed)
+    };
+
+    class SWE_API MouseButtonReleasedEvent : public MouseButtonEvent
+    {
+    public:
+        MouseButtonReleasedEvent(int button) : MouseButtonEvent(button) {}
+
+        std::string ToString() const override
+        {
+            std::stringstream ss;
+            ss << "MouseButtonReleasedEvent: " << m_Button;
+            return ss.str();
+        }
+
+        EVENT_CLASS_TYPE(MouseButtonReleased)
+    };
+}

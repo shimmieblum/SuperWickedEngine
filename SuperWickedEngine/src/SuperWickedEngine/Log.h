655f40907248fabb618056d3c6968288ae21ec3f
shimmieblum <shimmieblum@gmail.com>
2022-12-30 13:32:39 +0000

    rm unnecessary inlines

--- SuperWickedEngine/src/SuperWickedEngine/Log.h
+++ SuperWickedEngine/src/SuperWickedEngine/Log.h
@@ -8,8 +8,8 @@ namespace SuperWickedEngine
     public:
         static void Init();
 
-        inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
-        inline static std::shared_ptr<spdlog::logger>& GetClientLogger(){ return s_ClientLogger; }
+        static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+        static std::shared_ptr<spdlog::logger>& GetClientLogger(){ return s_ClientLogger; }
     private:
         static std::shared_ptr<spdlog::logger> s_CoreLogger;
         static std::shared_ptr<spdlog::logger> s_ClientLogger;

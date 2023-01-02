fae9497363513874b15f2542b668772988478cf2
shimmieblum <shimmieblum@gmail.com>
2023-01-02 16:18:04 +0000

    added assertion macro

--- SuperWickedEngine/src/SuperWickedEngine/Core.h
+++ SuperWickedEngine/src/SuperWickedEngine/Core.h
@@ -10,4 +10,13 @@
     #error SuperWickedEngine only works on windows
 #endif
 
+#ifdef SWE_ENABLE_ASSERTS
+    #define SWE_ASSERT( x, ...) { if(!(x)) { SWE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+    #define SWE_CORE_ASSERT( x, ...) { if(!(x)) { SWE_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+    #define SWE_ASSERT(x, ...)
+    #define SWE_CORE_ASSERT( x, ...)
+#endif
+
+
 #define BIT(x) (1 << (x)) // bitfield

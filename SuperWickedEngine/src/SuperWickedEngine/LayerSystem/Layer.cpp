203e141bdc706a41a627a3449d910dc79def784d
shimmieblum <shimmieblum@gmail.com>
2023-03-12 18:26:14 +0000

    added layer system with layerstack

---
+++ SuperWickedEngine/src/SuperWickedEngine/LayerSystem/Layer.cpp
@@ -0,0 +1,12 @@
+﻿#include "swepch.h"
+#include "Layer.h"
+
+namespace SuperWickedEngine
+{
+    Layer::Layer(const std::string& debugName)
+        : m_DebugName(debugName) { }
+
+    Layer::~Layer() { }
+
+
+}
---
+++ SuperWickedEngine/src/SuperWickedEngine/LayerSystem/Layer.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "SuperWickedEngine/Core.h"
+#include "SuperWickedEngine/Events/Event.h"
+
+
+namespace SuperWickedEngine {
+
+// To implement a Layer simply implement this header file. 
+    class SWE_API Layer
+    {
+    public:
+        Layer(const std::string& debugName = "Layer");
+        virtual ~Layer();
+            
+        virtual void OnAttach() {}
+        virtual void OnDetach() {}
+        virtual void OnUpdate() {}
+        virtual void OnEvent(Event& event) {}
+
+        [[nodiscard]] const std::string& GetName() const { return m_DebugName; }
+    protected:
+        std::string m_DebugName;
+    };
+}
